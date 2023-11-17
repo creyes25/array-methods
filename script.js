441c9feb0e3cb56f65ebabd90a8e2fc0c62649b9
creyes25 <carolina.reyes2022@gmail.com>
2023-11-16 19:45:14 -0600

    create the _fill method

--- script.js
+++ script.js
@@ -219,7 +219,14 @@ Array.prototype._splice = function (start = 0, deleteCount, ...items) {
 /// Fill
 const numbers = [1, 2, 3, 4, 5];
 
-Array.prototype._fill = undefined;
+Array.prototype._fill = function (value, start = 0, end = this.length) {
+  if (start < 0) start += this.length;
+  if (end < 0) end += this.length;
+
+  for (let i = start; i < end; i++) {
+    this[i] = value;
+  }
+};
 
 // runTests(() => {
 //   if (!Array.prototype._fill) {
